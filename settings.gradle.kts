32a356d3f7e8e5363da75c44eb8eda26e878f013
ilclo <131272565+ilclo@users.noreply.github.com>
2025-09-20 10:06:51 +0200

    ddd

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prov" // o il nome che vuoi
+include(":app")
