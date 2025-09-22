aaaa4c575e4e805d143c927508cb8fa981a4ff67
ilclo <131272565+ilclo@users.noreply.github.com>
2025-09-22 17:17:27 +0200

    fff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+pluginManagement {
+  plugins {
+    id("com.android.application") version "8.5.2"
+    id("com.android.library")     version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
+  }
 }
 
 android {
